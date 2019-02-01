1bbf3b5918c96131b587857002d7eb4c49b2a449
vagrant <nathalang_t@hotmail.ca>
2019-02-01 18:56:12 +0000

    create ajax post request to /polls

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,30 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+$(document).ready(function () {
+  console.log("loading app.js")
+  const adminData = {
+    admin: {
+      name: "Aqua",
+      email: "poggers@email.c",
+      question: "what should we eat?",
+      choices: [{ choice1: "description" }, { choice2: "description" }, { choice3: "description" }]
     }
-  });;
-});
+  }
+
+
+
+  //create onsubmit event handler prevent default
+  $("form.question").on("submit", function (event) {
+    event.preventDefault();
+    console.log("submit");
+
+    $.ajax({
+      method: "POST",
+      url: "/polls",
+      data: adminData,
+    }).done((links) => {
+      console.log(links)
+    });
+
+  })
+
+
+})
