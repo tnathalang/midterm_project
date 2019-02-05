cc3a008fd58738c3b4d0c5dad36b87cc213c89fe
qwhat <justinmao@live.ca>
2019-02-05 21:19:19 +0000

    app.js adds data to database

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -20,25 +20,6 @@ $(document).ready(function () {
     event.preventDefault();
     console.log("submit");
 
-
-
-    $.ajax({
-      method: "POST",
-      url: "/api/polls",
-      data: {
-        name_poll: "Generic question name to be change eventually",
-        question: $("#exampleFormControlTextarea1").val(),
-        admin_id: 1,
-        choices: []
-      }
-    }).done((id) => {
-      console.log(id)
-      localStorage.setItem('poll_id', id)
-
-    });
-
-
-    //
     //creates the html for the choices
     createQuestion()
     makeOptions()
@@ -48,37 +29,57 @@ $(document).ready(function () {
     $("form.choiceArea").on("submit", function (event) {
       event.preventDefault();
 
-      const options = {
-        method: "POST",
-        url: `/api/polls/${localStorage.getItem('poll_id')}`,
-        dataType: "JSON",
-        data: {
-          options: $("textarea.choiceOption").val(),
-          description: $("textarea.choiceDesc").val(),
-          choice_number: $("ul.choices").children("li").length + 1
-        }
-
-      }
-
-      request(options, function () {
         createOption()
-      })
-
-
     })
 
     // adds the poll to the database
     $("form.pollCreation").on("submit", function (event) {
       event.preventDefault();
 
+      const askQuestion = $("h2.question").text()
+      const choiceArr = []
+      const choices = $("ul.choices").children("li")
+
+      for (let i = 0; i < choices.length; i++){
+        const name = $(choices[i]).children("span.name").text()
+        const descript = $(choices[i]).children("span.desc").text()
+        const choiceObj = {
+          name_choice: name,
+          description: descript,
+          choice_number: i + 1,
+        }
+        choiceArr.push(choiceObj);
+      }
       const options = {
-        method: "GET",
-        url: "api/polls/1/choices",
+        url: "api/polls",
+        method: "POST",
+        data:{
+          name_poll:"placeholder name",
+          question: askQuestion,
+          admin_id: "1",
+          choices: choiceArr
+        }
       }
 
-      request(options, function (results) {
+      request(options, function(){})
 
-        console.log(results)
+
+      // options: $("textarea.choiceOption").val(),
+      // description: $("textarea.choiceDesc").val(),
+      // choice_number: $("ul.choices").children("li").length + 1
+      //
+      // const options = {
+      //   method: "POST",
+      //   url: "api/polls",
+      //   dataType:"JSON",
+      //   data: {
+      //
+      //   }
+      // }
+
+      // request(options, function(results){
+      //
+      //   console.log(results)
 
         // const admin = JSON.stringify(results[0].admin_link)
         // const submit = JSON.stringify(results[0].submit_link)
@@ -90,9 +91,9 @@ $(document).ready(function () {
         //
         // $("main.container").empty()
         // $("main.container").append(links)
-
-
-      })
+      //
+      //
+      // })
 
 
     })
@@ -114,7 +115,7 @@ $(document).ready(function () {
   // haven't properly test it out yet
   $('.test').on("click", (event) => {
     const options = {
-      url: `/ api / polls / 1 / choices`,
+      url: `/api/polls/1/choices`,
       method: "GET",
       datatype: "JSON"
     };
@@ -145,22 +146,29 @@ $(document).ready(function () {
 
 function createOption() {
   const choiceText = $("textarea.choiceOption").val();
-  const $choiceInput = $("<li>").text(choiceText);
+  const $choiceInput = $("<li>");
+  const $choiceText = $("<span>").text(choiceText).addClass("name");
+  const choiceDesc = $("input.choiceDesc").val();
+  const $choiceDesc = $("<span>").text(choiceDesc).addClass("desc");
+  $($choiceInput).append($choiceText);
+  $($choiceInput).append($choiceDesc);
   $(".choices").append($choiceInput);
   $(".choiceOption").val("")
+  $(".choiceDesc").val("")
 }
 
 function createQuestion() {
   const question = $("textarea.form-control").val();
   $(".option-container").empty();
   $(".form-control").val("");
-  const load = $("<h2>").text(question);
+  const load = $("<h2>").text(question).addClass("question");
   $(".option-container").append(load);
 }
 function makeOptions() {
   const $choiceArea = $("<form>").addClass("choiceArea");
   $("<ul>").addClass("choices").appendTo($choiceArea);
   $("<textarea>").attr("placeholder", "add option").addClass("choiceOption").appendTo($choiceArea);
+  $("<input>").attr("type", "text").addClass("choiceDesc").appendTo($choiceArea);
   $("<button>").attr("type", "submit").text("add choice").addClass("choice").appendTo($choiceArea);
   $(".option-container").append($choiceArea);
 }
